26096a2a7523f862e9d9d33b5e281c946103a77b
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2024-07-03 12:24:22 -0400

    Update data generation script

--- gen_data.py
+++ gen_data.py
@@ -7,36 +7,29 @@ import sys
 
 def run_TES():
     pindex_list = np.array(sys.argv[1:], dtype=float)
-    for p in pindex_list:
-        print(f"Generate data for p = {p}")
+    for pindex in pindex_list:
+        print(f"Generate data for p = {pindex}")
 
-        # Set minimum and maxmimum velocity dispersion
-        ts = tes.TES(p=p, rs=1e2)
-        sigma_min = ts.sigma
-        rfloor = ts.sonic_radius_floor()
-        ts = tes.TES(p=p, rs=rfloor)
-        sigma_max = ts.sigma - 1e-6  # avoid exceeding sigma_max by truncation error in log10
-
-        velocity_dispersions = np.logspace(np.log10(sigma_min), np.log10(sigma_max), 1024)
-        velocity_dispersions = np.insert(velocity_dispersions, 0, 0)
-        ucrit, rcrit, mcrit, rsonic = [], [], [], []
-        for sigma in velocity_dispersions:
-            ts = tes.TES(sigma=sigma, p=p)
-            ucrit.append(np.log(1/ts.rho(ts.rmax)))
-            rcrit.append(ts.rmax)
-            mcrit.append(ts.menc(ts.rmax))
-            rsonic.append(ts.rs)
-        ucrit = np.array(ucrit)
+        rs_min = tes.minimum_sonic_radius(pindex=pindex)
+        sonic_radius = np.logspace(np.log10(rs_min), 2, 10**4)
+        rcrit, mcrit, ucrit, sigma = [], [], [], []
+        for rsonic in sonic_radius:
+            ts = tes.TES(pindex=pindex, rsonic=rsonic)
+            rcrit.append(ts.rcrit)
+            mcrit.append(ts.mcrit)
+            ucrit.append(ts.ucrit)
+            sigma.append(ts.sigma)
         rcrit = np.array(rcrit)
         mcrit = np.array(mcrit)
-        rsonic = np.array(rsonic)
+        ucrit = np.array(ucrit)
+        sigma = np.array(sigma)
 
         res = dict(ucrit=ucrit,
                    rcrit=rcrit,
                    mcrit=mcrit,
-                   rsonic=rsonic,
-                   veldisp=velocity_dispersions)
-        fp = Path(__file__).parent / f"data/tsc.p{p}.p"
+                   rsonic=sonic_radius,
+                   sigma=sigma)
+        fp = Path(__file__).parent / f"data/tsc.p{pindex}.p"
         with open(fp, "wb") as handle:
             pickle.dump(res, handle)
 
