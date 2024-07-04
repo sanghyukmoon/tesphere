ce989b55cbbea74aa1c2c6271e9ce882fe32fd3b
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2024-07-04 13:11:48 -0400

    Add Logotrope

--- turb_sphere/tes.py
+++ turb_sphere/tes.py
@@ -254,6 +254,191 @@ class TES:
         return du, dlogm
 
 
+class Logotrope:
+    """Turbulent equilibrium sphere of a fixed central density.
+
+    xi = r/r_0 where r_0 = 3c_s/np.sqrt(4 pi G rho_c) following
+    McLaughlin & Pudritz (1996). This scale length is three times
+    the BE scale length.
+    """
+    def __init__(self, amp):
+        """
+        Parameters
+        ----------
+        amp : A_MP the parameter A of McLaughlin & Pudritz
+        """
+        self._rfloor = 1e-5
+        self._sigma = None
+        self.amp = amp
+        self.rcrit = self.critical_radius()
+        self.mcrit = self.enclosed_mass(self.rcrit)
+
+    def density(self, r):
+        """Calculate normalized density.
+
+        Notes
+        -----
+        rho = rho_c * e^u
+        """
+        u, _ = self.solve(r)
+        return np.exp(-u)
+
+    def velocity_dispersion(self, r):
+        r = np.atleast_1d(r)
+        u, _ = self.solve(r)
+        res = np.zeros(len(r))
+        sig2 = (1 - self.amp*u)*np.exp(u) - 1
+        np.sqrt(sig2, where=(sig2 >= 0), out=res)
+        return res
+
+    def enclosed_mass(self, r):
+        """
+        M = (c_s^2 * r_0 / G) * m
+        With this definition, rho_avg/rho_c = m / (3*r^3)
+        """
+        # If xi0 is inf, mass is also inf.
+        if isinstance(r, float) and np.isinf(r):
+            return np.inf
+        else:
+            mask = np.isinf(r)
+        u, du = self.solve(r)
+        m = self.amp*r**2*np.exp(u)*du
+        if not isinstance(r, float):
+            m[mask] = np.inf
+        # return scala when the input is scala
+        return m.squeeze()[()]
+
+    def total_pressure(self, r):
+        u, _ = self.solve(r)
+        return 1 - self.amp*u
+
+    @property
+    def sigma(self):
+        """Compute or return the velocity dispersion"""
+        if self._sigma is None:
+            self._sigma = self.compute_sigma()
+        return self._sigma
+
+    def compute_sigma(self):
+        """Calculate mass-weighted mean velocity dispersion.
+
+        Parameters
+        ----------
+        """
+        def _func(r):
+            rho = self.density(r)
+            sigma2 = self.velocity_dispersion(r)**2
+            return r**2*rho*sigma2
+        num, _ = quad(_func, 0, self.rcrit, epsrel=1e-6)
+        def _func(r):
+            rho = self.density(r)
+            return r**2*rho
+        den, _ = quad(_func, 0, self.rcrit, epsrel=1e-6)
+        return np.sqrt(num/den)
+
+    def solve(self, xi):
+        """Solve equilibrium equation
+
+        Parameters
+        ----------
+        xi : array
+            Dimensionless radii
+
+        Returns
+        -------
+        u : array
+            Log density u = log(rho/rho_c)
+        du : array
+            Derivative of u: d(u)/d(xi)
+        """
+        if isinstance(xi, float) and np.isinf(xi):
+            return np.inf, 0
+        else:
+            mask = np.isinf(xi)
+
+        xi = np.array(xi, dtype='float64')
+        y0 = np.array([0, 0])
+        if xi.min() > self._rfloor:
+            xi = np.insert(xi, 0, self._rfloor)
+            istart = 1
+        else:
+            istart = 0
+        if np.all(xi <= self._rfloor):
+            u = y0[0]*np.ones(xi.size)
+            du = y0[1]*np.ones(xi.size)
+        else:
+            y = odeint(self._dydx, y0, np.log(xi))
+            u = y[istart:, 0]
+            du = y[istart:, 1]/xi[istart:]
+
+        if not isinstance(xi, float):
+            u[mask] = np.inf
+            du[mask] = 0
+
+        # return scala when the input is scala
+        return u.squeeze()[()], du.squeeze()[()]
+
+    def critical_radius(self):
+        """Find critical TES radius
+
+        Returns
+        -------
+        float
+            Critical radius
+        """
+        xi = np.logspace(0, 3, 512)
+        kappa = self.get_bulk_modulus(xi)
+        idx = (kappa < 0).nonzero()[0]
+        if len(idx) < 1:
+            # kappa is everywhere positive, meaning that this TES
+            # is stable at every radius. Return inf.
+            return np.inf
+        else:
+            idx = idx[0] - 1
+        func = lambda x: self.get_bulk_modulus(10**x)
+
+        x0, x1 = np.log10(xi[idx]), np.log10(xi[idx+1])
+        try:
+            logrmax = newton(func, x0, x1=x1).squeeze()[()]
+        except:
+            logrmax = np.nan
+        return 10**logrmax
+
+    def get_bulk_modulus(self, xi):
+        # Perturbation with the fixed turbulent velocity field
+        # i.e., p = r_s = const.
+        u, du = self.solve(xi)
+        dpsi = self.amp*np.exp(u)*du
+        num = 1 - dpsi**2/18/(1 - self.amp*u)
+        den = 1 - dpsi / (9*xi)
+        kappa = num / den
+        return kappa
+
+    def _dydx(self, y, t):
+        """Differential equation for hydrostatic equilibrium.
+
+        Parameters
+        ----------
+        y : array
+            Vector of dependent variables
+        x : array
+            Independent variable
+
+        Returns
+        -------
+        array
+            Vector of (dy/dx)
+        """
+        y1, y2 = y
+        dy1 = y2
+
+        a = self.amp
+        b = 2*self.amp
+        c = -9*np.exp(2*t - 2*y1)
+        dy2 = -(b/a)*y2 - (c/a)
+        return np.array([dy1, dy2])
+
+
 def minimum_sonic_radius(pindex, rtol=1e-3):
     a = 1e-6
     b = 999
@@ -285,3 +470,11 @@ def find_sonic_radius(pindex, sigma_target):
                          " the steep dependence of rcrit on rsonic")
     rsonic = brentq(lambda x: get_sigma(pindex, x) - sigma_target, rs_min, 1e5)
     return rsonic
+
+
+def find_logotrope_amp(sigma_target):
+    def _func(A):
+        ts = Logotrope(A)
+        return ts.sigma - sigma_target
+    amp = brentq(_func, 0.15, 1)
+    return amp
