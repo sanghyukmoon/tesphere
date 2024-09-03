ac876a214b486cb842148d132a3ca98f0fd8a586
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2024-09-03 15:47:59 -0400

    Refactoring; adding TESe class

--- turb_sphere/tes.py
+++ turb_sphere/tes.py
@@ -1,5 +1,5 @@
 from scipy.integrate import quad, odeint
-from scipy.optimize import brentq, newton
+from scipy.optimize import brentq, newton, minimize_scalar
 import numpy as np
 
 
@@ -19,6 +19,12 @@ class TES:
         power-law index of the velocity dispersion.
     rsonic : float
         dimensionless sonic radius.
+    ucrit : float
+        critical (logarithmic) center-to-edge pressure contrast.
+    rcrit : float
+        critical radius.
+    mcrit : float
+        critical mass.
     """
     def __init__(self, pindex=0.5, rsonic=np.inf, compute_rcrit=True):
         self._rfloor = 1e-5
@@ -43,7 +49,7 @@ class TES:
     def velocity_dispersion(self, r):
         return (r / self.rsonic)**(self.pindex)
 
-    def enclosed_mass(self, xi0):
+    def enclosed_mass(self, r):
         """Calculates dimensionless enclosed mass.
 
         The dimensionless mass enclosed within the dimensionless radius xi
@@ -53,7 +59,7 @@ class TES:
 
         Parameters
         ----------
-        xi0 : float
+        r : float
             Radius within which the enclosed mass is computed. If None, use
             the maximum radius of a sphere.
 
@@ -62,17 +68,18 @@ class TES:
         float
             Dimensionless enclosed mass.
         """
-        # If xi0 is inf, mass is also inf.
-        if isinstance(xi0, float) and np.isinf(xi0):
+        # If r is inf, mass is also inf.
+        if isinstance(r, float) and np.isinf(r):
             return np.inf
         else:
-            mask = np.isinf(xi0)
-        u, du = self.solve(xi0)
-        chi = self.chi(xi0)
-        dchi = 2*self.pindex*(chi - 1)/xi0
-        m = xi0**2*(chi*du - dchi)
-        if not isinstance(xi0, float):
+            mask = np.isinf(r)
+        _, du = self.solve(r)
+        chi = self.chi(r)
+        dchi = 2*self.pindex*(chi - 1)/r
+        m = r**2*(chi*du - dchi)
+        if not isinstance(r, float):
             m[mask] = np.inf
+
         # return scala when the input is scala
         return m.squeeze()[()]
 
@@ -248,6 +255,39 @@ class TES:
 
         return du, dlogm
 
+    @staticmethod
+    def minimum_sonic_radius(pindex, rtol=1e-3):
+        a = 1e-6
+        b = 999
+
+        def func(x):
+            ts = TES(pindex=pindex, rsonic=x)
+            return ts.rcrit
+
+        while ((b - a)/a > rtol):
+            c = 10**(0.5*(np.log10(a) + np.log10(b)))
+            if np.isinf(func(c)):
+                a = c
+            else:
+                b = c
+        return b
+
+    @staticmethod
+    def find_sonic_radius(pindex, sigma_target):
+        if sigma_target == 0:
+            return np.infty
+
+        def get_sigma(pindex, rsonic):
+            ts = TES(pindex=pindex, rsonic=rsonic)
+            return ts.sigma
+        rs_min = TES.minimum_sonic_radius(pindex=pindex)
+        ts = TES(pindex=pindex, rsonic=rs_min)
+        if ts.sigma < sigma_target:
+            raise ValueError(f"sigma = {sigma_target:.2f} is too large."
+                             " Cannot find the critical radius due to"
+                             " the steep dependence of rcrit on rsonic")
+        rsonic = brentq(lambda x: get_sigma(pindex, x) - sigma_target, rs_min, 1e5)
+        return rsonic
 
 class Logotrope:
     """Turbulent equilibrium sphere of a fixed central density.
@@ -302,6 +342,42 @@ class Logotrope:
             m[mask] = np.inf
         # return scala when the input is scala
         return m.squeeze()[()]
+
+    def critical_radius(self):
+        """Find critical TES radius
+
+        Returns
+        -------
+        float
+            Critical radius
+        """
+        xi = np.logspace(0, 3, 512)
+        kappa = self.bulk_modulus(xi)
+        idx = (kappa < 0).nonzero()[0]
+        if len(idx) < 1:
+            # kappa is everywhere positive, meaning that this TES
+            # is stable at every radius. Return inf.
+            return np.inf
+        else:
+            idx = idx[0] - 1
+        func = lambda x: self.bulk_modulus(10**x)
+
+        x0, x1 = np.log10(xi[idx]), np.log10(xi[idx+1])
+        try:
+            logrmax = newton(func, x0, x1=x1).squeeze()[()]
+        except:
+            logrmax = np.nan
+        return 10**logrmax
+
+    def bulk_modulus(self, xi):
+        # Perturbation with the fixed turbulent velocity field
+        # i.e., p = r_s = const.
+        u, du = self.solve(xi)
+        dpsi = self.amp*np.exp(u)*du
+        num = 1 - dpsi**2/18/(1 - self.amp*u)
+        den = 1 - dpsi / (9*xi)
+        kappa = num / den
+        return kappa
 
     def total_pressure(self, r):
         u, _ = self.solve(r)
@@ -374,42 +450,6 @@ class Logotrope:
         # return scala when the input is scala
         return u.squeeze()[()], du.squeeze()[()]
 
-    def critical_radius(self):
-        """Find critical TES radius
-
-        Returns
-        -------
-        float
-            Critical radius
-        """
-        xi = np.logspace(0, 3, 512)
-        kappa = self.get_bulk_modulus(xi)
-        idx = (kappa < 0).nonzero()[0]
-        if len(idx) < 1:
-            # kappa is everywhere positive, meaning that this TES
-            # is stable at every radius. Return inf.
-            return np.inf
-        else:
-            idx = idx[0] - 1
-        func = lambda x: self.get_bulk_modulus(10**x)
-
-        x0, x1 = np.log10(xi[idx]), np.log10(xi[idx+1])
-        try:
-            logrmax = newton(func, x0, x1=x1).squeeze()[()]
-        except:
-            logrmax = np.nan
-        return 10**logrmax
-
-    def get_bulk_modulus(self, xi):
-        # Perturbation with the fixed turbulent velocity field
-        # i.e., p = r_s = const.
-        u, du = self.solve(xi)
-        dpsi = self.amp*np.exp(u)*du
-        num = 1 - dpsi**2/18/(1 - self.amp*u)
-        den = 1 - dpsi / (9*xi)
-        kappa = num / den
-        return kappa
-
     def _dydx(self, y, t):
         """Differential equation for hydrostatic equilibrium.
 
@@ -434,44 +474,299 @@ class Logotrope:
         dy2 = -(b/a)*y2 - (c/a)
         return np.array([dy1, dy2])
 
+    @staticmethod
+    def find_amplitude(sigma_target):
+        def _func(A):
+            ts = Logotrope(A)
+            return ts.sigma - sigma_target
+        amp = brentq(_func, 0.15, 1)
+        return amp
 
-def minimum_sonic_radius(pindex, rtol=1e-3):
-    a = 1e-6
-    b = 999
+class TESe:
+    """Turbulent equilibrium sphere of a fixed external pressure.
 
-    def func(x):
-        tsc = TES(pindex=pindex, rsonic=x)
-        return tsc.rcrit
+    Parameters
+    ----------
+    pindex : float, optional
+        power-law index of the velocity dispersion.
+    rsonic : float, optional
+        dimensionless sonic radius.
 
-    while ((b - a)/a > rtol):
-        c = (a + b)/2
-        if np.isinf(func(c)):
-            a = c
+    Attributes
+    ----------
+    pindex : float
+        power-law index of the velocity dispersion.
+    rsonic : float
+        dimensionless sonic radius.
+    ucrit : float
+        critical (logarithmic) center-to-edge pressure contrast.
+    rcrit : float
+        critical radius.
+    mcrit : float
+        critical mass.
+    """
+    def __init__(self, pindex=0.5, rsonic=np.inf, compute_ucrit=True):
+        self._rfloor = 1e-5
+        self._rceil = 1e20
+        self._uceil = 20
+        self._sigma = None
+        self.pindex = pindex
+        self.rsonic = rsonic
+        if compute_ucrit:
+            self.ucrit = self.critical_contrast()
+            self.rcrit = self.find_rmax(self.ucrit)
+            self.mcrit = self.enclosed_mass(self.rcrit, self.ucrit)
+
+    def get_min_xi_s(self, atol=1e-4):
+        a = 1e-5
+        b = 1e3
+
+        def f(x):
+            tse = TESe(pindex=self.pindex, rsonic=x)
+            try:
+                uc, rc, mc = tse.get_crit()
+            except ValueError:
+                rc = np.infty
+            return rc
+
+        while (b - a > atol):
+            c = (a + b)/2
+            if np.isinf(f(c)):
+                a = c
+            else:
+                b = c
+        return b
+
+    def density(self, r, u0):
+        """Calculate normalized density.
+
+        Notes
+        -----
+        rho/(P_ext/c_s^2) = exp(u) / chi
+        """
+        u, _ = self.solve(r, u0)
+        return np.exp(u) / self.chi(r)
+
+    def velocity_dispersion(self, r):
+        return (r / self.rsonic)**(self.pindex)
+
+    def enclosed_mass(self, r, u0):
+        """Calculates dimensionless enclosed mass.
+
+        The dimensionless mass enclosed within the dimensionless radius xi
+        is defined by
+            M(r) = G^{3/2}P_ext^{1/2}c_s^{-4}m(r)
+
+        Parameters
+        ----------
+        r : float
+            Radius within which the enclosed mass is computed. If None, use
+            the maximum radius of a sphere.
+
+        Returns
+        -------
+        float
+            Dimensionless enclosed mass.
+        """
+        _, du = self.solve(r, u0)
+        chi = self.chi(r)
+        m = -r**2*chi*du
+
+        # return scala when the input is scala
+        return m.squeeze()[()]
+
+    def chi(self, r):
+        """Dimensionless function chi = 1 + (dv/cs)^2
+
+        Equivalent to the ratio of the total to thermal pressure.
+        """
+        return 1 + self.velocity_dispersion(r)**2
+
+    @property
+    def sigma(self):
+        """Compute or return the velocity dispersion"""
+        if self._sigma is None:
+            self._sigma = self.compute_sigma()
+        return self._sigma
+
+    def compute_sigma(self):
+        """Calculate mass-weighted mean velocity dispersion within rcrit
+
+        Returns
+        -------
+        sigv : float
+            Mass-weighted radial velocity dispersion
+        """
+        def func(r):
+            rho = self.density(r, self.ucrit)
+            sigma2 = self.velocity_dispersion(r)**2
+            return r**2*rho*sigma2
+        num, _ = quad(func, self._rfloor, self.rcrit)
+
+        def func(r):
+            rho = self.density(r, self.ucrit)
+            return r**2*rho
+        den, _ = quad(func, self._rfloor, self.rcrit)
+
+        sigv = np.sqrt(num/den)
+        return sigv
+
+    def solve(self, r, u0):
+        """Solve equilibrium equation
+
+        Parameters
+        ----------
+        r : array
+            Dimensionless radii
+        u0 : float
+            Dimensionless logarithmic central density
+
+        Returns
+        -------
+        u : array
+            Log density u = log(rho/rho_e)
+        du : array
+            Derivative of u: d(u)/d(r)
+        """
+        r = np.array(r, dtype='float64')
+        y0 = np.array([u0, 0])
+        if r.min() > self._rfloor:
+            r = np.insert(r, 0, self._rfloor)
+            istart = 1
         else:
-            b = c
-    return b
+            istart = 0
+        if np.all(r <= self._rfloor):
+            u = y0[0]*np.ones(r.size)
+            du = y0[1]*np.ones(r.size)
+        else:
+            y = odeint(self._dydx, y0, np.log(r))
+            u = y[istart:, 0]
+            du = y[istart:, 1]/r[istart:]
 
+        # return scala when the input is scala
+        return u.squeeze()[()], du.squeeze()[()]
 
-def find_sonic_radius(pindex, sigma_target):
-    if sigma_target == 0:
-        return np.infty
+    def _dydx(self, y, t):
+        """Differential equation for hydrostatic equilibrium.
 
-    def get_sigma(pindex, rsonic):
-        ts = TES(pindex=pindex, rsonic=rsonic)
-        return ts.sigma
-    rs_min = minimum_sonic_radius(pindex=pindex)
-    ts = TES(pindex=pindex, rsonic=rs_min)
-    if ts.sigma < sigma_target:
-        raise ValueError(f"sigma = {sigma_target:.2f} is too large."
-                         " Cannot find the critical radius due to"
-                         " the steep dependence of rcrit on rsonic")
-    rsonic = brentq(lambda x: get_sigma(pindex, x) - sigma_target, rs_min, 1e5)
-    return rsonic
+        Parameters
+        ----------
+        y : array
+            Vector of dependent variables
+        x : array
+            Independent variable
 
+        Returns
+        -------
+        array
+            Vector of (dy/dx)
+        """
+        y1, y2 = y
+        dy1 = y2
+        x = np.exp(t)
 
-def find_logotrope_amp(sigma_target):
-    def _func(A):
-        ts = Logotrope(A)
-        return ts.sigma - sigma_target
-    amp = brentq(_func, 0.15, 1)
-    return amp
+        chi = self.chi(x)
+        dchi = 2*self.pindex*(chi - 1)
+        ddchi = 2*self.pindex*dchi
+
+        a = chi
+        b = chi + dchi
+        c = 4*np.pi*np.exp(2*t + y1) / chi
+        dy2 = -(b/a)*y2 - (c/a)
+        return np.array([dy1, dy2])
+
+    def find_rmax(self, u0):
+        """Calculates the dimensionless radial extent of a TES.
+
+        Find the boundary where P_eff = P_ext
+
+        Returns
+        -------
+        float
+            Dimensionless radial extent
+
+        Raises
+        ------
+        ValueError
+            when radius exceeds the allowed range [xi_min, xi_max]
+        """
+        logrmax = brentq(lambda x: self.solve(10**x, u0)[0],
+                         np.log10(self._rfloor), np.log10(self._rceil))
+        return 10**logrmax
+
+    def critical_contrast(self):
+        """Finds critical TES parameters.
+
+        Critical point is defined as a maximum point in the R-M curve.
+
+        Returns
+        -------
+        u_c : float
+            Critical logarithmic central density
+        r_c : float
+            Critical radius
+        m_c : float
+            Critical mass
+
+        Notes
+        -----
+        The pressure at a given mass is P = pi^3 c_s^8 / (G^3 M^2) m^2, so
+        the minimization is done with respect to m^2.
+        """
+        # bracket the roots
+        u0s = np.linspace(0.1, self._uceil)  # u_be = 2.64
+        menc = []
+        for u0 in u0s:
+            rmax = self.find_rmax(u0)
+            menc.append(self.enclosed_mass(rmax, u0))
+        logm = np.log10(menc)
+        dmdu = np.gradient(logm, u0s)
+        idx = (dmdu < 0).nonzero()[0]
+        if len(idx) < 1:
+            raise ValueError("Cannot find maximum mass")
+        else:
+            idx = idx[0] - 1
+        x0, x1 = u0s[idx-1], u0s[idx+1]
+
+        def func(u0):
+            rmax = self.find_rmax(u0)
+            menc = self.enclosed_mass(rmax, u0)
+            return -menc
+        res = minimize_scalar(func, bracket=[x0, x1], bounds=(x0, x1))
+        return res.x
+
+    @staticmethod
+    def minimum_sonic_radius(pindex, rtol=1e-2):
+        a = 1e-6
+        b = 999
+
+        def func(rs):
+            ts = TESe(pindex=pindex, rsonic=rs)
+            return ts.rcrit
+
+        while ((b - a)/a > rtol):
+            # Doing bisection in log space is a bit faster
+            c = 10**(0.5*(np.log10(a) + np.log10(b)))
+            try:
+                TESe(pindex=pindex, rsonic=c)
+                b = c
+            except ValueError:
+                a = c
+        return b
+
+    @staticmethod
+    def find_sonic_radius(pindex, sigma_target):
+        if sigma_target == 0:
+            return np.infty
+
+        def get_sigma(pindex, rsonic):
+            ts = TESe(pindex=pindex, rsonic=rsonic)
+            return ts.sigma
+        rs_min = TESe.minimum_sonic_radius(pindex=pindex)
+        ts = TES(pindex=pindex, rsonic=rs_min)
+        if ts.sigma < sigma_target:
+            raise ValueError(f"sigma = {sigma_target:.2f} is too large."
+                             " Cannot find the critical radius due to"
+                             " the steep dependence of rcrit on rsonic")
+        rsonic = brentq(lambda x: get_sigma(pindex, x) - sigma_target, rs_min, 1e5)
+        return rsonic
